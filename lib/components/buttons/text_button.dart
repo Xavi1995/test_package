ad13dab9a31ed2e541becacef995c15c5f6c00d1
Xavier Castro <xavier.castro@smarting.es>
2024-04-30 08:13:18 +0200

    Added textColor property to a SmaTextButton component

--- lib/components/buttons/text_button.dart
+++ lib/components/buttons/text_button.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 
 class SmaTextButton extends StatelessWidget {
-  const SmaTextButton(
-      {super.key,
-      required this.text,
-      required this.onTap,
-      this.backgroundColor});
+  const SmaTextButton({
+    super.key,
+    required this.text,
+    required this.onTap,
+    this.backgroundColor,
+    this.textColor,
+  });
 
   final String text;
   final Color? backgroundColor;
+  final Color? textColor;
   final Function? onTap;
 
   @override
@@ -16,12 +19,17 @@ class SmaTextButton extends StatelessWidget {
     return GestureDetector(
       onTap: () => onTap,
       child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        decoration: BoxDecoration(
-          color: backgroundColor,
-          borderRadius: BorderRadius.circular(20),
-        ),
-      ),
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          decoration: BoxDecoration(
+            color: backgroundColor,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Text(
+            text,
+            style: TextStyle(
+              color: textColor,
+            ),
+          )),
     );
   }
 }
